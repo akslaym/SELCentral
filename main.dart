64092599149affd5ce73a33ea45e1403e0319599
archanaa131 <84088255+archanaa131@users.noreply.github.com>
2022-03-16 18:50:15 -0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,83 +8,77 @@ void main() {
       home: Home()
   ));
 }
-  
-  class Home extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-       appBar: AppBar(
-        title: Text('login'),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Container(
-        color: Colors.indigoAccent[100],
-        width: double.infinity,
-        height: double.infinity,
 
-       //Text('I am a...', style: TextStyle(fontSize: 35)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('login'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Container(
+          color: Colors.indigoAccent[100],
+          width: double.infinity,
+          height: double.infinity,
 
-           // Text('I am a...'),
-            //Text('I am a...', style: TextStyle(fontSize: 35)),
-            SizedBox(width: 10),
 
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.indigoAccent,
-                  borderRadius: const BorderRadius.all(const Radius.circular(20.0))
-                ),
-                //color: Colors.indigoAccent,
-                padding: EdgeInsets.all(35.0),
-                //margin: EdgeInsets.all(70),
-                width: 35,
-                height: 130,
-                child: Text('Student', style: TextStyle(color : Colors.white,fontSize: 18), textAlign: TextAlign.center),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+
+              // Text('I am a...'),
+
+              Text('     Login', style: TextStyle(color: Colors.white, fontSize: 35)),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.indigoAccent,
+                      borderRadius: const BorderRadius.all(const Radius.circular(30.0))
+                  ),
+                  //color: Colors.indigoAccent,
+                  padding: EdgeInsets.all(23.0),
+                  margin: EdgeInsets.all(40),
+                  height: 60,
+                  width: 190,
+
+                  child: Text('Username', style: TextStyle(color : Colors.white70),textAlign: TextAlign.center,)
+
 
 
               ),
-            ),
-            SizedBox(width: 10),
+              SizedBox(height: 1),
 
-            Expanded(
-              child: Container(
+              Container(
                   decoration: BoxDecoration(
-                      color: Colors.green[500],
-                      borderRadius: const BorderRadius.all(const Radius.circular(20.0))
+                      color: Colors.indigoAccent,
+                      borderRadius: const BorderRadius.all(const Radius.circular(30.0))
                   ),
                   //color: Colors.green[500],
-                  padding: EdgeInsets.all(35.0),
-                  width: 35,
-                  height: 130,
-                  child: Text('Teacher', style: TextStyle(color : Colors.white, fontSize: 18),textAlign: TextAlign.center,)
+                  padding: EdgeInsets.all(23.0),
+                  margin: EdgeInsets.all(40),
+                  height: 60,
+                  width: 190,
+                  child: Text('Password', style: TextStyle(color : Colors.white70),textAlign: TextAlign.center),
               ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              child: Container(
+              //SizedBox(height: 1),
+              Text('                       Forgot Password?', style: TextStyle(color: Colors.white70)),
+
+
+              Container(
                   decoration: BoxDecoration(
                       color: Colors.pinkAccent[100],
                       borderRadius: const BorderRadius.all(const Radius.circular(20.0))
                   ),
                   //color: Colors.pinkAccent[100],
-                  padding: EdgeInsets.all(35.0),
-                  width: 35,
-                  height: 130,
-                  child: Text('New Account', style: TextStyle(color : Colors.white, fontSize: 18),textAlign: TextAlign.center),
+                  padding: EdgeInsets.all(50.0),
+                  child: Text('Login', style: TextStyle(color : Colors.white)),
               ),
-            ),
-            SizedBox(width: 10),
-          ],
+            ],
+          ),
+
         ),
-
-      ),
-
-
-  );
-    }
+    );
+  }
 }
 
